a51000ee7b829e40431c79bc4f4b1098cf4da54e
Árni Dagur <19227577+ArniDagur@users.noreply.github.com>
2017-11-26 00:30:30 +0000

    Update overcast-volume.user.js

--- overcast-volume.user.js
+++ overcast-volume.user.js
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
     var vs_div = document.querySelector("html body div.content.pure-u-1 div.pure-g div.pure-u-1.pure-u-sm-3-5 .fullart_container");
-    if (vs_div !== null) {
+    if (vs_div) {
         vs_div.innerHTML += '<div><div class="smallcaps centertext">Volume: <span id="volumenr">1</span></div><input id="volumecontrol" type="range" min="0" max="1" step="0.01" value="1"></input></div>';
         var vs_player = document.getElementById('audioplayer');
         var vs_volcontrol = document.getElementById('volumecontrol');
